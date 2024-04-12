8f826477888e74dc053f173fc36e110e01689515
Premjith J <164313576+Premjith99@users.noreply.github.com>
2024-04-12 18:28:39 +0530

    Add files via upload

---
+++ i2cmaster2slave.c
@@ -0,0 +1,50 @@
+#include<pic18.h>
+void i2c_init()
+{
+TRISC4=TRISC3=1;
+SSPSTAT=0x80;//1000
+SSPCON1=0x28;//
+SSPCON2=0x00;
+SSPADD=49;
+}
+void i2c_start()
+{
+SEN=1;//start condition send
+while(SEN==1);//sen(last bit of sspcon2)
+}
+void i2c_stop()
+{
+PEN=1;//stop condition enable bit
+while(PEN==1);//
+}
+void i2c_data(char a)
+{
+SSPIF=0;
+SSPBUF=a;
+while(SSPIF==0);
+}
+char i2c_read(char a)
+{
+RCEN=1;//receive 
+while(RCEN);//
+ACKDT=a;//1- NACK before stopping 0->ack
+ACKEN=1;
+while(ACKEN);
+return SSPBUF;
+}
+void main()
+{
+	int i;
+for(i=0;i<=10000;i++);
+i2c_init();
+i2c_start();//start slavead+R/w  
+i2c_data(0x32);//0x32
+i2c_data('a');
+i2c_data('g');
+i2c_stop();
+while(1) 	
+{
+
+}
+}
+
---
+++ slave2.c
@@ -0,0 +1,63 @@
+/*
+ * File:   slave2.c
+ * Author: Premjith J
+ *
+ * Created on January 12, 2024, 9:48 AM
+ */
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+void i2c_init();
+char i2c_statusw();
+void trans(char a)
+{
+	while((UCSRA&0x20)==0);//UDR empty when 5th bit in UCSRA is 1
+	UDR=a;
+}
+void i2c_statusr(char a);
+int main(void)
+{
+	char a;
+
+
+
+	DDRD=0xff;
+
+	i2c_init();
+	i2c_statusr('a');
+	i2c_statusr('g');
+	while(1)
+	{
+
+	}
+}
+
+void i2c_init()
+{
+	DDRC=0;//1111 1111
+	TWAR=0x32;// 0001 000 0   0110 1000 0x61
+
+	TWCR=0x44;//ack
+
+	TWCR|=0x80;//TWCR=TWCR|10
+	while((TWCR&(0x80))==0);//slave address
+}
+
+char i2c_statusw()//master to slave
+{
+	TWCR=0x44;//
+	TWCR|=0x80;
+	while((TWCR&(0x80))==0);//
+	return TWDR;
+	//1 clear flag bit lpc2138/avr 0 1
+}
+
+void i2c_statusr(char a)
+{
+	TWDR=a;
+	TWCR=0x44;//
+	TWCR|=0x80;
+	while((TWCR&(0x80))==0);
+	//1 clear flag bit lpc2138/avr 0 1
+}
